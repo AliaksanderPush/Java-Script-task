cb0732fafcce104f0d92c58bfeb5589a03ea1f12
Sash <tribeca1983@gmail.com>
2022-01-17 10:55:34 +0300

    feth

--- AJAX/juerry.js
+++ AJAX/juerry.js
@@ -1,5 +1,5 @@
 
-
+/*
 function testLoad() {
 	$.ajax('https://jsonplaceholder.typicode.com/todos/',
 	{type:'GET', dataType:'json', success: dataLoaded, error:errorHandler } 
@@ -10,47 +10,20 @@ function dataLoaded(data) {
  	console.log(data)
 	
 }
-
-function errorHandler(jqXHR, statusStr, errorStr) {
-   console.log(statusStr+' '+errorStr)
-}
+*/
 
 
 
 
 const url = "https://fe.it-academy.by/AjaxStringStorage2.php";
-const updatePassword=Math.random(); 
-const stringName='PUSHNOV_PROJECT_AVIATICKETS';
+let updatePassword; 
+const stringName='PUSHNOV_PROJECT_FOOTBALL'; 
 
 
-const info = {age:28}
+const info = {age:455}
 
-function storeInfo() {
-    $.ajax( {
-            url : url, type : 'POST', cache : false, dataType:'json',
-            data : { f : 'LOCKGET', n : stringName, p : updatePassword },
-            success :(data) => lockGetReady(data) , error : errorHandler
-        }
-    );
-}
-        
-function lockGetReady(callresult) {
-    if ( callresult.error!=undefined )
-        alert(callresult.error);
-    else {
-        
-        $.ajax( {
-                url : url, type : 'POST', cache : false, dataType:'json',
-                data : { f : 'UPDATE', n : stringName, v : JSON.stringify(info), p : updatePassword },
-                success : (data) => updateReady(data), error : errorHandler
-            }
-        );
-    }
-}
 
-function updateReady(callresult) {
-    if (callresult.error != undefined) console.log(callresult.error);
-  }
+
 
   
 
@@ -58,29 +31,44 @@ function updateReady(callresult) {
 async function getInfo() {
 	const url = "https://fe.it-academy.by/AjaxStringStorage2.php";
 	let sp = new URLSearchParams();
-    sp.append('f', 'READ');
-    sp.append('n', "PUSHNOV_PROJECT_AVIATICKETS");
-
+    updatePassword=Math.random()
+    sp.append('f', 'LOCKGET');
+    sp.append('n', "PUSHNOV_PROJECT_FOOTBALL");
+    sp.append('p', updatePassword) 
+     
 	try {
         let response=await fetch(url,{method: 'post', body: sp });
-        let data=await response.json();
-        console.log(data);
+        let data = await response.json();
+        lockGetReady(data);
     }
     catch ( error ) {
         console.error(error);
     }
 }
-//sendInfo()
+
+async function lockGetReady(callresult) {
+	const url = "https://fe.it-academy.by/AjaxStringStorage2.php";
+	let sp = new URLSearchParams();
+    sp.append('f', 'UPDATE');
+    sp.append('n', "PUSHNOV_PROJECT_FOOTBALL");
+    sp.append('p', updatePassword);
+    sp.append('v',JSON.stringify(info)); 
+ 
+	try {
+        let response=await fetch(url,{method: 'post', body:  sp });
+        updateReady(response);
+    }
+    catch ( error ) {
+        console.error(error);
+    }
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
+function updateReady(callresult) {
+    console.log(callresult)
+    if ( callresult.error!=undefined )
+        alert(callresult.error);
+}
+function errorHandler(jqXHR,statusStr,errorStr) {
+    alert(statusStr+' '+errorStr);
+}
